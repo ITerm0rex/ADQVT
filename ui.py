e5762ea923ed915be7d54dc8dc470da936bbaaed
ITerm0rex <ITerm0rex@gmail.com>
2023-12-28 12:16:35 +0100

    Further experimentation.
    Co-authored-by: Lina <linagrigoriev@users.noreply.github.com>
    Co-authored-by: Yoshui <Yoshui@users.noreply.github.com>

--- ui.py
+++ ui.py
@@ -1,8 +1,9 @@
 # %%
 import asyncio
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import json
 import os
+import sys
 import threading
 from typing import Any, Awaitable, Callable, Coroutine, Union
 import numpy as np
@@ -17,7 +18,7 @@ import prototype
 from nicegui.functions.refreshable import _P, _T
 
 
-def Idempotence(f: ui.refreshable[_P, _T]):
+def Idempotence(f: ui.refreshable[_P, _T]):  # type: ignore
     flag = True
 
     def wrap(*args: _P.args, **kwargs: _P.kwargs):
@@ -32,46 +33,31 @@ def Idempotence(f: ui.refreshable[_P, _T]):
 
 
 def stop():
-    app.shutdown()
     app.stop()
-    exit(0)
+    app.shutdown()
+    # if app.native.main_window:
+    #     app.native.main_window.destroy()
+    # exit(0)
+
+
+# from nicegui.logging import log
+
+# log.warning("aaaaaaaa")
+
+# app.native.window_args
 
 
 @dataclass
-class env:
-    file_names: set[str]
+class env_fact:
+    file_names: set[str] = field(default_factory=set[str])
     Class: str = "Visibility"
     Hold: str = "Visibility"
 
-
-# class Ide:#(ui.refreshable[_P, _T]):
-#     flag = True
-#     f: ui.refreshable[_P, _T]
-
-#     # Union[_T, Coroutine[None, None, _T]]:
-#     def __init__(self, f: ui.refreshable[_P, _T]):
-#         self.f = f
-
-#     def __call__(
-#         self, *args: _P.args, **kwargs: _P.kwargs
-#     ) -> _T | Awaitable[_T] | None:
-#         if self.flag:
-#             self.flag = False
-#             return self.f(*args, **kwargs)
-#         return self.f.refresh(*args, **kwargs)
-#         # return self.run(*args, **kwargs)
-
-#     # def run(self, *args: _P.args, **kwargs: _P.kwargs):
-#     #     if self.flag:
-#     #         self.flag = False
-#     #         return self.f(*args, **kwargs)
-#     #     return self.f.refresh(*args, **kwargs)
-
+env = env_fact()
 
 @Idempotence
 @ui.refreshable
 def show_plot():
-    # await asyncio.sleep(2)
     with ui.row() as row:
         row.classes("absolute-right")
         # row.classes("row reverse")
@@ -99,12 +85,12 @@ def show_plot():
 
 
 class UIApp:
-    def __init__(self):
-        # self.file_names = set()
-        self.select = None
-        self.row = None
-        self.plt = None
-        self.flag = True
+    # def __init__(self):
+    #     # self.file_names = set()
+    #     self.select = None
+    #     self.row = None
+    #     self.plt = None
+    #     self.flag = True
 
     async def choose_file(self):
         if app.native.main_window:
@@ -113,29 +99,6 @@ class UIApp:
             ):
                 env.file_names.clear()
                 env.file_names.update(files)
-                # self.file_names.update(files)
-
-                # self.file_names.clear()
-                # self.file_names += list(files)
-
-                # print(show_plot.instance)
-
-                # show_plot()
-
-                # if self.flag:
-                #     self.flag = False
-                #     show_plot(self.file_names)
-                # else:
-                #     show_plot.refresh(self.file_names)
-
-                # self.update_selected_files()
-
-                # for file in files:
-                #     print(file)
-                #     ui.label(file)
-                #     ui.notify(file)
-        # if self.select:
-        #     self.select.set_options(self.file_names, value=self.file_names[0])
 
     def create_button(self, text, on_click_handler):
         with ui.button(text, on_click=on_click_handler) as button:
@@ -149,132 +112,76 @@ class UIApp:
             return select
 
     def run_app(self):
-        # with ui.row() as row:
-        #     row.classes("absolute-right")
+        # env.file_names = set()
 
-        #     row.tailwind.background_color("green-400")
-        #     # row.tailwind.position("absolute")
-        #     # row.tailwind.top_right_bottom_left()
+        with ui.button("Choose File") as button:
+            # button.tailwind.background_color("green-100")
+            # button.style("background-color: green;")
+            button.on("click", self.choose_file)
 
-        #     with ui.pyplot(figsize=(3, 2)) as g:
-        #         prototype.gaze(self.file_names[0]).show()
+        # with ui.button("maxxx") as button:
+        #     # button.tailwind.background_color("green-100")
+        #     # button.style("background-color: green;")
 
-        #         g.on("click", lambda x: app.shutdown())
+        #     def maxx():
+        #         # print((app.native.main_window), sep="\n")
+        #         if app.native.main_window:
+        #             app.native.main_window.signal_server_shutdown()
+        #             # app.native.main_window.maximized = True
 
-        # self.update_selected_files()
+        #     button.on("click", maxx)
 
-        # .tailwind.background_color("red-100")
+        with ui.select([]) as fi:
+            # fi.bind_value(env, "file_names", forward=list)
+            fi.on("input", lambda: fi.set_options(list(env.file_names)))
 
-        # app.native.start_args["debug"] = True
-        # if app.native.main_window:
-        #     app.native.main_window.maximized = True
+        with ui.row() as row:
+            row.classes("absolute-right orange-6")
+            with ui.select([1, 2, 3]) as fi:
+                fi.classes("absolute-center")
 
-        # core.app.native.start_args["maximized"] = True
-
-        # opti = {
-        #     "maximized": True
-        # }
-
-        # app.native.window_args["maximized"] = True
-
-        # pathf = r".\HCI-2023\Annotated_Data_JSON\V3\event_json_data\s2_v3_-_matchmaker_scene\Annotation_s2_v3_-_matchmaker_scene.json"
-        # # pathf = r".\HCI-2023\Annotated_Data_JSON\V3\event_json_data\s4_v3_-_macbeth_opening_scene\Annotation_s4_v3_-_macbeth_opening_scene.json"
-
-        # if isinstance(v := show_plot([pathf]), Coroutine):
-        #     print(asyncio.run(v))
-        # else:
-        #     print(v)
-
-        ui.run(
-            title="test",
-            native=True,
-            fullscreen=False,
-            window_size=(1300, 900),
-            reload=False,
-            on_air=None,
-            # frameless=True
-            # *opti
+        self.create_selection("Class").set_options(
+            ["Visibility", "Gaze", "HumanAction", "Saccade", "Attention"]
         )
 
+        self.create_selection("Hold").set_options(
+            ["Visibility", "looking_at", "speaking", "markup", "face", "torso"]
+        )
 
-if __name__ == "__main__":
+        show_plot()
+
+        # ui.button("[exit]", on_click=stop).classes("absolute-top-right bg-red")
+        # with ui.button("[exit]", on_click=stop) as but:
+        #     but.classes("absolute-top-right bg-red")
+
+        with ui.button("Render") as button:
+            # button.on("click", lambda:print(env.file_names))
+            button.on("click", show_plot)
+
+        try:
+            ui.run(
+                title="demo",
+                native=True,
+                fullscreen=False,
+                window_size=(1300, 900),
+                reload=False,
+                on_air=None,
+                # frameless=True
+                # *opti
+            )
+        except asyncio.exceptions.CancelledError:
+            pass
+
+
+if __name__ in {"__main__", "__mp_main__"}:
     ui_app = UIApp()
-    env.file_names = set()
 
-    # choose_file_button = ui_app.create_button("Choose File", ui_app.choose_file)
+    # class aaa:
+    #     qqq = 3
+    #     abc = lambda self, x: self.qqq * x
 
-    with ui.button("Choose File") as button:
-        # button.tailwind.background_color("green-100")
-        # button.style("background-color: green;")
-        button.on("click", ui_app.choose_file)
+    #     def bbb(self):
+    #         print(self.abc(3))
 
-    # with ui_app.create_selection("file_names") as fi:
-    #     fi.on("click", lambda: fi.set_options(list(env.file_names)))
-
-    with ui.select([]) as fi:
-        # fi.bind_value(env, "file_names", forward=list)
-        fi.on("input", lambda: fi.set_options(list(env.file_names)))
-
-    ui_app.create_selection("Class").set_options(
-        ["Visibility", "Gaze", "HumanAction", "Saccade", "Attention"]
-    )
-    ui_app.create_selection("Hold").set_options(
-        ["Visibility", "looking_at", "speaking", "markup", "face", "torso"]
-    )
-
-    show_plot()
-
-    ui.button("[exit]", on_click=stop).classes("absolute-top-right bg-red")
-
-    with ui.button("Render") as button:
-        # button.on("click", lambda:print(env.file_names))
-        button.on("click", show_plot)
-
-    # select = ui.select(file_names, value=1)
-    # select.classes("w-200")
-
-    # ui_app.update_selected_files()
-
-    # with ui.button("hiiiii") as but:
-    #     but.on("click", lambda: )
-
-    # Arrange components
-    # with ui.header().classes(replace="row items-center") as header:
-    #     ui.button(on_click=lambda: left_drawer.toggle(), icon="menu").props(
-    #         "flat color=white"
-    #     )
-    #     with ui.tabs() as tabs:
-    #         ui.tab("A")
-    #         ui.tab("B")
-    #         ui.tab("C").classes("bg-green-100")
-
-    # with ui.left_drawer().classes("bg-blue-100") as left_drawer:
-    #     ui.label("Side menu")
-
-    # with ui.tab_panels(tabs, value="A").classes("w-full"):
-    #     with ui.tab_panel("A"):
-    #         ui.label("Content of A")
-    #     with ui.tab_panel("B"):
-    #         ui.label("Content of B")
-    #     with ui.tab_panel("C"):
-    #         ui.label("Content of C")
-
-    # [
-    #     r".\HCI-2023\Annotated_Data_JSON\V3\event_json_data\s2_v3_-_matchmaker_scene\Annotation_s2_v3_-_matchmaker_scene.json"
-    # ]
-
-    # tree = ui.tree(
-    #     [
-    #         {"id": "numbers", "icon": "tag", "children": [{"id": "1"}, {"id": "2"}]},
-    #         {
-    #             "id": "letters",
-    #             "icon": "text_fields",
-    #             "children": [{"id": "A"}, {"id": "B"}],
-    #         },
-    #     ],
-    #     label_key="id",
-    #     on_select=lambda e: ui.notify(e.value),
-    # )
-
-
+    # aaa().bbb()
     ui_app.run_app()
